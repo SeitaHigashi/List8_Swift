b7498777c564c9651d036c14979bbc2c51e4a0c8
SeitaHigashi <higashi110902@gmail.com>
2017-11-11 11:05:43 +0900

    FirstCommit

--- List8/main.swift
+++ List8/main.swift
@@ -6,7 +6,41 @@
 //  Copyright © 2017年 東誠太. All rights reserved.
 //
 
-import Foundation
+//import Foundation
+//import Cocoa
 
-print("Hello, World!")
-
+class PhoneBook{
+    private var data : [String:String] = [String:String]()
+    func add_data(){
+        print("New Name:")
+        let inname :String? = readLine()
+        print("Number")
+        let innumber :String? = readLine()
+        if inname != nil {
+            data[inname!] = innumber == nil ? "NoNumber" : innumber
+        }
+    }
+    func print_data(){
+        for (name,number) in data{
+            print("name:\(name)")
+            print("number:\(number)")
+        }
+    }
+    func delete_data(){
+        print("Delete_data name:")
+        let inname :String? = readLine()
+        if inname != nil {
+            data.removeValue(forKey: inname!)
+        }
+    }
+    func seach_data(){
+        
+    }
+}
+var data :PhoneBook = PhoneBook()
+switch readLine() {
+case "1"?:data.add_data()
+case "2"?:data.print_data()
+case "3"?:data.seach_data()
+default : break
+}
